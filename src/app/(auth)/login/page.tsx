ad8c2db5e9c9433dc6d66a911d65c5987eafd932
2pac <tarasyaremabcn@gmail.com>
2024-11-10 21:49:10 +0100

    logo login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -43,11 +43,11 @@ export default function LoginPage() {
   };
 
   return (
-    <Flex justify="center" align="center" vertical gap="middle">
-      <Image src={Logo} alt="OSS ATS Logo" width={128} height={128} />
+    <Flex justify="center" align="center" vertical gap="middle" style={{ height: '100vh' }}>
       <Typography.Title level={2}>
         The OSS ATS Platform
       </Typography.Title>
+      <Image src={Logo} alt="OSS ATS Logo" width={300} height={300} />
       <Typography.Text>
         Enter your admin password to access the platform!
       </Typography.Text>
