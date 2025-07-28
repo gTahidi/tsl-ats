07b06a3570b8a675b672c8f543b1ecc2b1d2b4ba
byte-rose <daniel@ujaotech.com>
2025-07-28 14:56:13 +0300

    replaced plausible with clarity

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -16,7 +16,15 @@ export default function Template({ children }: { children: React.ReactNode }) {
           <ClientLayout>{children}</ClientLayout>
         </Providers>
       </StyledComponentsRegistry>
-      <Script defer data-domain={process.env.DOMAIN} src="https://plausible.io/js/script.js" />
+      <Script id="microsoft-clarity" strategy="afterInteractive" dangerouslySetInnerHTML={{
+        __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID || 'slye7xus8v'}");
+        `,
+      }} />
     </div>
   );
 }
