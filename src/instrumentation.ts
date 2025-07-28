276ea42a300a171b89c0675393bab07a8192f58e
byte-rose <daniel@ujaotech.com>
2025-07-28 16:48:50 +0300

    fixed open telemetry

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,30 +1,5 @@
 import * as Sentry from '@sentry/nextjs';
-import { Context } from '@opentelemetry/api';
-import { Span as SpanInterface, SpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { registerOTel } from '@vercel/otel';
-
-/**
- * Span processor to reduce cardinality of span names.
- *
- * Customize with care!
- */
-class SpanNameProcessor implements SpanProcessor {
-  forceFlush(): Promise<void> {
-    return Promise.resolve();
-  }
-  onStart(span: SpanInterface, parentContext: Context): void {
-    // Reduce cardinality of span names for static assets
-    if (span.name.startsWith('GET /_next/static')) {
-      span.updateName('GET /_next/static');
-    } else if (span.name.startsWith('GET /_next/data')) {
-      span.updateName('GET /_next/data');
-    }
-  }
-  onEnd(): void {}
-  shutdown(): Promise<void> {
-    return Promise.resolve();
-  }
-}
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
@@ -33,7 +8,7 @@ export async function register() {
     // Register OpenTelemetry for Faro backend instrumentation
     registerOTel({
       serviceName: process.env.OTEL_SERVICE_NAME || 'unknown_service:node',
-      spanProcessors: ['auto', new SpanNameProcessor()],
+      spanProcessors: ['auto'],
     });
   }
 
